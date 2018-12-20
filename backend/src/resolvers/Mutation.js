9a897ed16e38749e1d5b91121b66035ba463c532
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-20 01:30:39 -0800

    added updated schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,7 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx, info) {
+        // create a dog
+    }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,7 @@
-const Query = {};
+const Query = {
+    dogs(parent, args, ctx, ) {
+        return [{name: 'Snickers'}, { name: 'Sunny'}]
+    }
+};
 
 module.exports = Query;
