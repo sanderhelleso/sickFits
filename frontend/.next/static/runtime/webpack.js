d9dea19951f0844bf0a665a239880ba20b91755f
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-21 18:32:51 -0800

    added view of sinel item

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88b5ad7503038ab510d2";
+/******/ 	var hotCurrentHash = "b79c523dd5efbbb81e99";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
