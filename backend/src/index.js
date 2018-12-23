8289b8e19e7c57a01bd76fee3c96d04e1229cc2d
Sanderhelleso <sanderhelleso@hotmail.com>
2018-12-22 21:24:14 -0800

    added JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,24 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer =  require('./createServer');
 const db = require('./db');
+const jwt = require('jsonwebtoken');
 
 const server = createServer();
 server.express.use(cookieParser());
 
-// TODO Use express middelware to populate current user
+// decode JWT to get user ID on each request
+server.express.use((req, res, next) => {
+    const { token } = req.cookies;
+    
+    // set token
+    if (token) {
+        const { userID } = jwt.verify(token, process.env.APP_SECRET);
+        req.userID = userID;
+    }
+
+    // continue
+    next();
+});
 
 // start server
 server.start({
--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "48ccbd326629a5a021d9";
+/******/ 	var hotCurrentHash = "b11a5e00fdc187543f6d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
